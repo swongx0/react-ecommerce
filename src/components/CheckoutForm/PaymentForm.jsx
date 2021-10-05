9e2111075b3f964343567ef3d88f93a49fa0d966
Sharon Wong <s.wongx0@gmail.com>
2021-10-05 00:09:16 -0700

    Address form and Payment form

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
-const PaymentForm = () => {
+import Review from './Review'
+
+const stripePromise = loadStripe('...');
+
+const PaymentForm = ({ checkoutToken, backStep }) => {
     return (
-        <div>
-            Payment Form
-        </div>
+        <>
+            <Review checkoutToken={checkoutToken} />
+            {/*
+            <Divider />
+            <Typography variant="h6" gutterBottom style={{margin: '20px 0'}}>Payment method</Typography>
+            <Elements stripe={stripePromise}>
+                <ElementConsumer>
+                    {({elements, stripe}) => (
+                        <form>
+                            <CardElement />
+                            <br /> <br />
+                            <div style={{display:'flex', justifyContent:'space-between'}}>
+                                <Button variant="outlined" onClick={backStep}>Back</Button>
+                                <Button type="submit" variant="contained" disabled={!stripe} color="primary">
+                                    Pay { checkoutToken.live.subtotal.formatted_with_symbol }
+                                </Button>
+
+                            </div>
+                        </form>
+                    )}
+                </ElementConsumer>
+            </Elements>*/}
+        </>
     )
 }
 
